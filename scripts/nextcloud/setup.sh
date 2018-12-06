b126a3100a62c9affd9cf2e0e598232e2cdc2e4a
Louis Chemineau <artonge@chmn.me>
2018-12-06 14:36:47 +0100

    Create mastodon files and some cleanup

--- scripts/nextcloud/setup.sh
+++ scripts/nextcloud/setup.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-# APPS
+# ENABLE APPS
 # docker-compose exec --user www-data nextcloud php occ app:enable user_ldap
 docker-compose exec --user www-data nextcloud php occ app:install calendar
 docker-compose exec --user www-data nextcloud php occ app:install contacts
 
-# BACKGROUND
+# CHOOSE CRON MODE
 docker-compose exec --user www-data nextcloud php occ background:cron
 
-# TRUSTED DOMAINS
+# SET TRUSTED DOMAINS
 docker-compose exec --user www-data nextcloud php occ config:system:set trusted_domains 0 --value files.flap.localhost
 
-# FUNCTIONNALITIES
+# DISABLE FUNCTIONNALITIES
 docker-compose exec --user www-data nextcloud php occ config:system:set updatechecker --value false --type boolean
 docker-compose exec --user www-data nextcloud php occ config:system:set upgrade.disable-web --value true --type boolean
 docker-compose exec --user www-data nextcloud php occ config:system:set appstoreenabled --value false --type boolean
 
-# REDIS
+# CONNECT TO REDIS
 docker-compose exec --user www-data nextcloud php occ config:system:set redis host --value redis
 docker-compose exec --user www-data nextcloud php occ config:system:set redis port --value 6379 --type integer
 docker-compose exec --user www-data nextcloud php occ config:system:set memcache.local --value '\OC\Memcache\Redis'
 docker-compose exec --user www-data nextcloud php occ config:system:set memcache.locking --value '\OC\Memcache\Redis'
-# docker-compose exec --user www-data nextcloud php occ config:system:set redis timeout --value 0.0 --type float
-# docker-compose exec --user www-data nextcloud php occ config:system:set redis password --value ''
-# docker-compose exec --user www-data nextcloud php occ config:system:set redis dbindex --value 0 --type integer
---
+++ scripts/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./scripts/nextcloud/setup.sh
+./scripts/mastodon/setup.sh
