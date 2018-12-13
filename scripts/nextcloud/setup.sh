6ef7fce68d190d4fe13d2db0f59079578a59b165
Louis Chemineau <artonge@chmn.me>
2018-12-13 14:37:54 +0100

    Add minimal LDAP

--- scripts/nextcloud/setup.sh
+++ scripts/nextcloud/setup.sh
@@ -3,7 +3,7 @@
 # set -e
 
 # ENABLE APPS
-# docker-compose exec --user www-data nextcloud php occ app:enable user_ldap
+docker-compose exec --user www-data nextcloud php occ app:enable user_ldap
 docker-compose exec --user www-data nextcloud php occ app:install calendar
 docker-compose exec --user www-data nextcloud php occ app:install contacts
 
@@ -23,3 +23,11 @@ docker-compose exec --user www-data nextcloud php occ config:system:set redis ho
 docker-compose exec --user www-data nextcloud php occ config:system:set redis port --value 6379 --type integer
 docker-compose exec --user www-data nextcloud php occ config:system:set memcache.local --value '\OC\Memcache\Redis'
 docker-compose exec --user www-data nextcloud php occ config:system:set memcache.locking --value '\OC\Memcache\Redis'
+
+# CONFIGURE LDAP
+docker-compose exec --user www-data nextcloud php occ ldap:create-empty-config
+docker-compose exec --user www-data nextcloud php occ ldap:set-config s01 ldapHost ldap
+docker-compose exec --user www-data nextcloud php occ ldap:set-config s01 ldapPort ldap
+docker-compose exec --user www-data nextcloud php occ ldap:set-config s01 ldapAgentName "cn=admin,dc=example,dc=org"
+docker-compose exec --user www-data nextcloud php occ ldap:set-config s01 ldapAgentPassword admin
+docker-compose exec --user www-data nextcloud php occ ldap:set-config s01 ldapBase "dc=example,dc=org"
