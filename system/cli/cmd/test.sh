c6966920e6b17b1c8d79af308bdff032b790079e
Louis Chemineau <louis@chmn.me>
2019-05-24 14:30:21 +0200

    Simplify test execution

--- system/cli/cmd/test.sh
+++ system/cli/cmd/test.sh
@@ -3,33 +3,28 @@
 set -e
 
 CMD=$1
-only=$2
+ARGS=($@)
+ARGS=${ARGS[@]:0}
+
+TEST_TO_RUN=$(ls $FLAP_DIR/system/cli/tests)
+
+if [ "$#" != "0" ]
+then
+    TEST_TO_RUN=$ARGS
+fi
 
 case $CMD in
-    ""|--only)
+    ""|*)
         echo "Running tests..."
-        if [ -f $FLAP_DIR/system/cli/tests/$only.spec.sh ]
-        then
-            test=$only.spec.sh
-            echo "  + Running '$test'..."
-            {
-                $FLAP_DIR/system/cli/tests/$test &&
-                echo "  ✅ All tests passes for '$test'."
-            } || {
-                echo "  ❌ Some tests failed for '$test'."
-                exit 1
-            }
 
-            exit 0
-        fi
-
-        for test in $(ls $FLAP_DIR/system/cli/tests)
+        for test in $TEST_TO_RUN
         do
-            if [ -f $FLAP_DIR/system/cli/tests/$test ]
+            test=$(basename $test .spec.sh)
+            if [ -f $FLAP_DIR/system/cli/tests/$test.spec.sh ]
             then
                 echo "  + Running '$test'..."
                 {
-                    $FLAP_DIR/system/cli/tests/$test &&
+                    $FLAP_DIR/system/cli/tests/$test.spec.sh &&
                     echo "  ✅ All tests passes for '$test'."
                 } || {
                     echo "  ❌ Some tests failed for '$test'."
@@ -41,7 +36,7 @@ case $CMD in
     summarize)
         echo "test | | Test manager's commands."
         ;;
-    help|*)
+    help)
         echo "test | [--only <test_suite>] | Test manager's commands." | column --table --separator "|"
         ;;
 esac
