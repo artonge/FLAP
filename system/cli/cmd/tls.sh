d17666301703af0471375c0675e5b5c0c4269966
Louis Chemineau <louis@chmn.me>
2019-06-05 16:08:51 +0200

    Update domain set logic

--- system/cli/cmd/tls.sh
+++ system/cli/cmd/tls.sh
@@ -6,37 +6,38 @@ CMD=$1
 
 case $CMD in
     generate)
-        DOMAIN_INFO=$(manager config show | grep DOMAIN_INFO | cut -d '=' -f2)
-
-        STATUS=$(echo $DOMAIN_INFO | grep -E ' (HANDLED)|(OK)|(ERROR)$' | cat)
-
-        # If the domain name has allready a status exit now, else continue and set the status to HANDLED.
-        if [ "$STATUS" != "" ]
+        # Only process when the request is WAITING
+        REQUEST_STATUS=$(cat $FLAP_DATA/domainRequestStatus.txt)
+        if [ "$REQUEST_STATUS" != "WAITING" ]
         then
-            echo $DOMAIN_INFO
             exit 0
         fi
 
-        echo "$DOMAIN_INFO HANDLED" > $FLAP_DATA/domainInfo.txt
+        # Set the request as HANDLING
+        echo "HANDLING" > $FLAP_DATA/domainRequestStatus.txt
+
         # Allow the server to pick up the status change.
         sleep 2
+
+        # Go to the flap directory to stop and start containers
         cd $FLAP_DIR
 
         {
             # Generate TLS certificates
+            REQUEST=$(cat $FLAP_DATA/domainRequest.txt) &&
             docker-compose down &&
-            $FLAP_DIR/system/cli/lib/certificates/generate_certs.sh $DOMAIN_INFO 2>&1 &&
-            echo "$DOMAIN_INFO OK" > $FLAP_DATA/domainInfo.txt &&
+            $FLAP_DIR/system/cli/lib/certificates/generate_certs.sh $REQUEST 2>&1 &&
+            echo $REQUEST > $FLAP_DATA/domainInfo.txt &&
+            echo "" > $FLAP_DATA/domainRequest.txt &&
+            echo "OK" > $FLAP_DATA/domainRequestStatus.txt &&
             manager config generate &&
             docker-compose up -d
         } || { # Catch error
             echo "Failed to generate certificates."
-            echo "$DOMAIN_INFO ERROR" > $FLAP_DATA/domainInfo.txt
+            echo "ERROR" > $FLAP_DATA/domainRequestStatus.txt
             docker-compose up -d
             exit 1
         }
-
-        echo "Certificates generated."
         ;;
     show)
         ls -1 /etc/letsencrypt/live
