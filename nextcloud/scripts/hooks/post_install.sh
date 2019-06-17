7c75ac4b26a6a530db0fc242f111de514a829fea
Louis Chemineau <louis@chmn.me>
2019-06-17 15:29:40 +0200

    Remove TTY from dc exec

--- nextcloud/scripts/hooks/post_install.sh
+++ nextcloud/scripts/hooks/post_install.sh
@@ -6,8 +6,8 @@ set -e
 $FLAP_DIR/nextcloud/scripts/wait_ready.sh
 
 # Run post install script for nextcloud
-docker-compose exec nextcloud chown www-data:www-data /data
-docker-compose exec nextcloud touch /data/.ocdata
+docker-compose -T exec nextcloud chown www-data:www-data /data
+docker-compose -T exec nextcloud touch /data/.ocdata
 
 # Generate config.php with the config
-docker-compose exec --user www-data nextcloud /generate_config.sh
+docker-compose -T exec --user www-data nextcloud /generate_config.sh
