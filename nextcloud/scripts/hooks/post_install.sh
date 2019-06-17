72bdcbb0fc77cece7afd586d787d4167aed742cc
Louis Chemineau <louis@chmn.me>
2019-06-17 15:37:32 +0200

    Fix docker cmd

--- nextcloud/scripts/hooks/post_install.sh
+++ nextcloud/scripts/hooks/post_install.sh
@@ -6,8 +6,8 @@ set -e
 $FLAP_DIR/nextcloud/scripts/wait_ready.sh
 
 # Run post install script for nextcloud
-docker-compose -T exec nextcloud chown www-data:www-data /data
-docker-compose -T exec nextcloud touch /data/.ocdata
+docker-compose exec -T nextcloud chown www-data:www-data /data
+docker-compose exec -T nextcloud touch /data/.ocdata
 
 # Generate config.php with the config
-docker-compose -T exec --user www-data nextcloud /generate_config.sh
+docker-compose exec -T --user www-data nextcloud /generate_config.sh
