867d69c282767f18189bc96c7fb741f39130b732
Louis Chemineau <louis@chmn.me>
2019-06-17 16:00:34 +0200

    Fix dc cmd in hooks

--- nextcloud/scripts/hooks/post_domain_update.sh
+++ nextcloud/scripts/hooks/post_domain_update.sh
@@ -6,4 +6,4 @@ set -e
 $FLAP_DIR/nextcloud/scripts/wait_ready.sh
 
 # Generate config.php with the new config
-docker-compose -T exec --user www-data nextcloud /generate_config.sh
+docker-compose exec -T --user www-data nextcloud /generate_config.sh
