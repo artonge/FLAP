0ec36855614d2f03ffc7e35f9e413f37dcfe24c9
Louis Chemineau <louis@chmn.me>
2019-07-31 17:05:37 +0200

    Change working dir in hooks cmd to allow dc cmds

--- system/cli/cmd/hooks.sh
+++ system/cli/cmd/hooks.sh
@@ -7,8 +7,14 @@ CMD=${1:-}
 case $CMD in
     post_install|post_update|post_domain_update|health_check)
         echo "Running $CMD hook:"
+        
+        # Go to FLAP_DIR to allow docker-compose cmds.
+        cd $FLAP_DIR
+
+        # Run the targeted hook for each service.
         for service in $(ls --directory $FLAP_DIR/*/)
         do
+            # Check if the hook exists
             if [ -f $service/scripts/hooks/$CMD.sh ]
             then
                 echo "  - $(basename $service)"
