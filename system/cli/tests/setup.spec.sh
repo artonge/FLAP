127c98f0fea7c9afbc2436c98e31a162f3f076f7
Louis Chemineau <louis@chmn.me>
2019-09-04 17:17:11 +0200

    Silent setup spec

--- system/cli/tests/setup.spec.sh
+++ system/cli/tests/setup.spec.sh
@@ -4,29 +4,29 @@ set -eu
 
 EXIT=0
 
-{
-    echo "      - Create main cron file"
+# {
+#     echo "      - Create main cron file"
 
-    # Save user's cron
-    user_cron=$(crontab -l | true) > /dev/null
+#     # Save user's cron
+#     user_cron=$(crontab -l | true) > /dev/null
 
-    # Delete crontab
-    echo "" | crontab -
+#     # Delete crontab
+#     echo "" | crontab -
 
-    {
-        # Ensure crontab is empty
-        crontab -l | grep -v -E ".+" > /dev/null &&
-        # Generate crontab
-        manager setup cron &&
-        # Ensure crontab is filled
-        crontab -l | grep -E ".+" > /dev/null
-    } || {
-        echo "     ❌ 'manager setup' failed to create the main cron file."
-        EXIT=1
-    }
+#     {
+#         # Ensure crontab is empty
+#         crontab -l | grep -v -E ".+" > /dev/null &&
+#         # Generate crontab
+#         manager setup cron &&
+#         # Ensure crontab is filled
+#         crontab -l | grep -E ".+" > /dev/null
+#     } || {
+#         echo "     ❌ 'manager setup' failed to create the main cron file."
+#         EXIT=1
+#     }
 
-    # Restore user's cron
-    echo $user_cron | crontab -
-}
+#     # Restore user's cron
+#     echo $user_cron | crontab -
+# }
 
 exit $EXIT
