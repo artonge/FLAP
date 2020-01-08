bdc8ebf9ba3534755cf9585bae0f83d5a7d7fe55
Louis Chemineau <louis@chmn.me>
2020-01-08 13:10:52 +0100

    Split system's migrations

---
+++ system/scripts/migrations/4.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+# Change manager's name for flapctl.
+ln -sf $FLAP_DIR/system/cli/flapctl.sh /bin/flapctl
+
+# Run post_install script for mail.
+mkdir -p $FLAP_DATA/mail/data
+cat $FLAP_DIR/mail/scripts/migrations/base_migration.txt > $FLAP_DATA/mail/current_migration.txt
+$FLAP_DIR/mail/scripts/hooks/post_install.sh
+
+# Open ports
+flapctl setup network
+
+# Regenerate TLS certificates for new subdomain: mail.{...}.
+flapctl tls generate
+
+# Uninstall postfix and dovecot.
+apt remove -y postfix dovecot
+apt purge -y postfix dovecot
+apt autoremove
+
+# Increase compose timeout for slow connexion.
+echo "export COMPOSE_HTTP_TIMEOUT=120" >> /etc/environment
