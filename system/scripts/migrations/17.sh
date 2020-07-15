5b5d5959cdaaca9430bb1acd311b393a3fa69648
Louis Chemineau <louis@chmn.me>
2020-07-15 17:45:06 +0200

    Fix migration 17

--- system/scripts/migrations/17.sh
+++ system/scripts/migrations/17.sh
@@ -8,7 +8,7 @@ echo "* [17] Generating TLS certificates to include home and exclude core."
 flapctl tls generate
 
 echo "* [17] Deleting legacy core submodule."
-rm -rf "$FLAP_DATA/core"
+rm -rf "$FLAP_DIR/core"
 
 echo "* [17] Mooving core' data to home."
 mv "$FLAP_DATA/core" "$FLAP_DATA/home"
