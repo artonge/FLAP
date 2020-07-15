20b08fd75cab8767c34b6a28b476203a7e004f9e
Louis <1160639-artonge@users.noreply.gitlab.com>
2020-07-15 14:33:23 +0000

    Add documentation for backup and restore logic + Move markdown file to docs folder + Split and improve installation doc

---
+++ system/scripts/migrations/17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+# Version v1.12.0
+
+echo "* [17] Generating TLS certificates to include home and exclude core."
+flapctl tls generate
+
+echo "* [17] Deleting legacy core submodule."
+rm -rf "$FLAP_DATA/core"
+
+echo "* [17] Mooving core' data to home."
+mv "$FLAP_DATA/core" "$FLAP_DATA/home"
