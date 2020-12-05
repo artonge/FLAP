1127ddac5b7c2d0f6232b46c4ab7abb0b66ad51d
Louis Chemineau <louis@chmn.me>
2020-12-05 11:37:22 +0100

    Fix monitoring.flap.test discovery during pipeline

--- system/debug_scripts/pipeline_simulator.sh
+++ system/debug_scripts/pipeline_simulator.sh
@@ -50,6 +50,7 @@ docker run \
 	--add-host="weblate.flap.test:$FLAP_IP" \
 	--add-host="analytics.flap.test:$FLAP_IP" \
 	--add-host="video.flap.test:$FLAP_IP" \
+	--add-host="monitoring.flap.test:$FLAP_IP" \
 	docker:stable \
 	sh
 
@@ -85,6 +86,7 @@ docker run \
 	--add-host="weblate.flap.test:$FLAP_IP" \
 	--add-host="analytics.flap.test:$FLAP_IP" \
 	--add-host="video.flap.test:$FLAP_IP" \
+	--add-host="monitoring.flap.test:$FLAP_IP" \
 	--volume /var/run/docker.sock:/var/run/docker.sock \
 	--volume /flap_dir:/flap_dir \
 	--volume /flap_data:/flap_data \
