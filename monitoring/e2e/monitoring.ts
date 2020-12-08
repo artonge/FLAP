0af5417affca9fa41e09a662d424b1cd9dbeca04
Louis Chemineau <louis@chmn.me>
2020-12-08 14:53:02 +0100

    Improve monitoring e2e tests

--- monitoring/e2e/monitoring.ts
+++ monitoring/e2e/monitoring.ts
@@ -4,11 +4,14 @@ Feature("monitoring")
 
 Scenario("launch monitoring", (I) => {
 	I.amOnPage(`https://monitoring.${process.env.PRIMARY_DOMAIN_NAME}`)
+
+	I.waitForText("Welcome to Grafana")
+
 	I.fillField("user", "admin")
 	I.fillField("password", process.env.ADMIN_PWD)
 	I.click("Log in")
 
-	I.see("Welcome to Grafana")
+	I.waitForText("Dashboards")
 
 	I.amOnPage(`https://monitoring.${process.env.PRIMARY_DOMAIN_NAME}/?search=open`)
 	I.see("Docker Containers")
