3b23c980dbd0e085fd988f984cc8b1cb722fc6e1
Louis Chemineau <louis@chmn.me>
2020-12-11 17:27:38 +0100

    Bump matomo to v4 + Update nginx.conf + Add migration #1

--- matomo/scripts/migrations/1.sh
+++ matomo/scripts/migrations/1.sh
@@ -4,5 +4,26 @@ set -eu
 
 # Version v1.14.2
 
-echo "* [1] Migrate database to utf8mb4"
-docker-compose run --rm --user www-data matomo php /var/www/html/console config:set core:convert-to-utf8mb4
+echo "* [1] Start matomo."
+docker-compose run --name migrate_matomo --detach matomo
+
+echo "* [1] Fix email encryption setting."
+docker exec migrate_matomo php /var/www/html/console config:set --section="mail" --key="encryption" --value="tls"
+
+echo "* [1] Update matomo."
+docker exec --user root migrate_matomo apt update
+docker exec --user root migrate_matomo apt install -y rsync
+docker exec migrate_matomo rsync \
+	--delete \
+	--archive \
+	--exclude "/plugins/" \
+	--exclude "/config/config.ini.php" \
+	/usr/src/matomo/ /var/www/html
+
+docker exec migrate_matomo php /var/www/html/console core:update
+
+echo "* [1] Migrate database to utf8mb4."
+docker exec migrate_matomo php /var/www/html/console core:convert-to-utf8mb4
+
+echo "* [1] Stop ldap and mariadb."
+docker-compose down
