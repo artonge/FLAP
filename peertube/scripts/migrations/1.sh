871e7fee1d6c5704bd594439879587820287464f
Louis Chemineau <louis@chmn.me>
2021-01-06 14:07:43 +0100

    Update peertube saml plugin config template to enable singleLogout

--- peertube/scripts/migrations/1.sh
+++ peertube/scripts/migrations/1.sh
@@ -6,5 +6,17 @@ set -eu
 
 echo "* [1] Update saml2 plugin to version 0.0.2."
 docker-compose run -T peertube npm run plugin:install -- --npm-name peertube-plugin-auth-saml2 --plugin-version 0.0.2
-# Stop started services associated to peertube.
+
+echo "* [1] Update auth-saml2 plugin config."
+saml_config=$(jq \
+	--null-input \
+	--arg provider_cert "$(cat "$FLAP_DATA/lemon/saml/cert.pem")" \
+	--arg service_cert "$(cat "$FLAP_DATA/peertube/saml/cert.pem")" \
+	--arg service_priv_key "$(cat "$FLAP_DATA/peertube/saml/private_key.pem")" \
+	--from-file "$FLAP_DIR/peertube/config/saml_config.jq"
+)
+
+docker-compose exec -T --user postgres postgres psql peertube --command "UPDATE public.plugin SET settings='$saml_config' WHERE name='auth-saml2';"
+
+echo "* [1] Stop started services."
 flapctl stop
