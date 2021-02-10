fd444fc52bf61d298ea2f0cc7ae1efe4ccbf594b
Louis Chemineau <louis@chmn.me>
2021-02-10 12:24:44 +0100

    Un fix peertube's saml plugin version

--- peertube/scripts/migrations/2.sh
+++ peertube/scripts/migrations/2.sh
@@ -4,19 +4,8 @@ set -eu
 
 # Version v1.14.4
 
-echo "* [1] Update saml2 plugin to version 0.0.4."
-docker-compose run -T peertube npm run plugin:install -- --npm-name peertube-plugin-auth-saml2 --plugin-version 0.0.4
+echo "* [2] Un fix peertube's saml plugin version."
+docker-compose run -T peertube npm run plugin:install -- --npm-name peertube-plugin-auth-saml2
 
-echo "* [1] Update auth-saml2 plugin config."
-saml_config=$(jq \
-	--null-input \
-	--arg provider_cert "$(cat "$FLAP_DATA/lemon/saml/cert.pem")" \
-	--arg service_cert "$(cat "$FLAP_DATA/peertube/saml/cert.pem")" \
-	--arg service_priv_key "$(cat "$FLAP_DATA/peertube/saml/private_key.pem")" \
-	--from-file "$FLAP_DIR/peertube/config/saml_config.jq"
-)
-
-docker-compose exec -T --user postgres postgres psql peertube --command "UPDATE public.plugin SET settings='$saml_config' WHERE name='auth-saml2';"
-
-echo "* [1] Stop started services."
+echo "* [2] Stop started services."
 flapctl stop
