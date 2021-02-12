d0ed8201c911cb2970941c8adbead228a23aa42e
Louis Chemineau <louis@chmn.me>
2021-02-12 18:11:41 +0100

    Create env var for peertube saml plugin version

---
+++ peertube/scripts/hooks/post_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+
+echo "Update SAML auth plugin."
+docker-compose exec -T peertube npm run plugin:install -- --npm-name peertube-plugin-auth-saml2 --plugin-version "$PEERTUBE_SAML_PLUGIN_VERSION"
