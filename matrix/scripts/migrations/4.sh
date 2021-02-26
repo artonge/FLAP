25ca2783bd18372e121b63d83759aedbb5f94e9c
Louis Chemineau <louis@chmn.me>
2021-02-26 15:53:34 +0100

    Update homeserver.yml in matrix's migration #4 to remove template_dir property

--- matrix/scripts/migrations/4.sh
+++ matrix/scripts/migrations/4.sh
@@ -5,4 +5,19 @@ set -eu
 # Version 1.14.6
 
 echo "* [4] Update Synapse's SAML metadata."
-flapctl exec matrix hooks/post_domain_update
+# flapctl exec matrix hooks/post_domain_update
+
+echo "* [4] Update homeserver.yml to remove templates"
+synapse_final_config="$FLAP_DATA/matrix/synapse/data/homeserver.yaml"
+config_files=("$synapse_final_config" "$FLAP_DIR/matrix/config/synapse.yaml")
+
+echo "Merging synapse.yaml with homeserver.yaml."
+# shellcheck disable=SC2016
+yq \
+	--yaml-output \
+	--yaml-roundtrip \
+	--slurp \
+	'reduce .[] as $config ({}; . * $config)' "${config_files[@]}" \
+		> "$synapse_final_config".tmp
+
+mv "$synapse_final_config".tmp "$synapse_final_config"
