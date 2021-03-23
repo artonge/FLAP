3e2fdab2443dbad37d27b45157741c29b3f79ff9
Louis Chemineau <louis@chmn.me>
2021-03-23 14:54:16 +0100

    Silent shellcheck error

--- system/cli/lib/load_env_vars.sh
+++ system/cli/lib/load_env_vars.sh
@@ -6,6 +6,7 @@ mkdir --parents "$FLAP_DATA/system"
 
 # shellcheck source=system/flapctl.examples.d/pipeline.env
 # shellcheck disable=SC1091
+# shellcheck disable=SC1094
 source "$FLAP_DATA/system/flapctl.env"
 
 # Export global environnement variables.
