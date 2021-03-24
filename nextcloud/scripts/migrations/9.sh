57c8cdfffc7d2d973eef367c400ae89d50f8010e
Louis Chemineau <louis@chmn.me>
2021-03-24 15:01:04 +0100

    Update nextcloud's plugins in migration#9

--- nextcloud/scripts/migrations/9.sh
+++ nextcloud/scripts/migrations/9.sh
@@ -9,4 +9,8 @@ flapctl hooks wait_ready nextcloud
 echo "* [9] Enable Nextcloud app store."
 docker exec --user www-data flap_nextcloud php occ config:system:set appstoreenabled --value true --type boolean
 
+echo "* [9] Update Nextcloud's plugins."
+docker exec --user www-data flap_nextcloud php occ app:update --all
+docker exec --user www-data flap_nextcloud php occ app:enable user_saml
+
 docker-compose --no-ansi down
