4bd71244c157e8eb809e7875ff8e634eecd206d1
Louis Chemineau <louis@chmn.me>
2021-04-01 14:02:41 +0200

    Load load_env.sh file only for enabled services

--- system/scripts/hooks/load_env.sh
+++ system/scripts/hooks/load_env.sh
@@ -2,16 +2,9 @@
 
 set -eu
 
-FLAP_ENV_VARS="$FLAP_ENV_VARS \${ADMIN_PWD} \${ARCH} \${FLAP_VERSION}"
+FLAP_ENV_VARS="$FLAP_ENV_VARS \${ADMIN_PWD}"
 # 22 - SSH
 NEEDED_PORTS="$NEEDED_PORTS 22/tcp"
 
 export ADMIN_PWD
 ADMIN_PWD=$(generatePassword system admin_pwd)
-
-export ARCH
-ARCH=$(uname -m)
-
-cd "$FLAP_DIR"
-export FLAP_VERSION
-FLAP_VERSION=$(git describe --tags --abbrev=0)
