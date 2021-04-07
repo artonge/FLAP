7c9f2357e2c6e52048a915d3cae822603d849e17
Louis Chemineau <louis@chmn.me>
2021-04-07 16:05:18 +0200

    WIP 2

--- mailman/config/web/settings_local.template.py
+++ mailman/config/web/settings_local.template.py
@@ -1,3 +1,4 @@
+# Change indexing engine.
 # https://github.com/maxking/docker-mailman/blob/master/README.md#setting-up-search-indexing
 HAYSTACK_CONNECTIONS = {
     'default': {
@@ -6,30 +7,54 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
+
 # Disable social login.
-MAILMAN_WEB_SOCIAL_AUTH = []
+INSTALLED_APPS = [
+    'hyperkitty',
+    'postorius',
+    'django_mailman3',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'django_gravatar',
+    'compressor',
+    'haystack',
+    'django_extensions',
+    'django_q',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+]
+
+
+# Change "From:" e-mail address
+DEFAULT_FROM_EMAIL="admin@$PRIMARY_DOMAIN_NAME"
+
 
 # LDAP Auth.
 import ldap
 from django_auth_ldap.config import LDAPSearch, LDAPSearchUnion
 
-
 AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
-    'django_auth_ldap.backend.LDAPBackend',
 )
 
-AUTH_LDAP_SERVER_URI = "ldap"
+AUTH_LDAP_SERVER_URI = "ldap://ldap"
 AUTH_LDAP_BIND_DN = "cn=admin,dc=flap,dc=local"
 AUTH_LDAP_BIND_PASSWORD = "$ADMIN_PWD"
 AUTH_LDAP_START_TLS = False
 
 AUTH_LDAP_USER_ATTR_MAP = {"username": "uid", "first_name": "cn", "email": "mail"}
-AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=flap,dc=local ", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=users,dc=flap,dc=local", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
 
-AUTH_LDAP_USER_FLAGS_BY_GROUP = {
-    "is_superuser": "organizationalPerson"
-}
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-DEBUG = True
+# Debug
+DEBUG = False
--- mailman/scripts/hooks/post_restore.sh
+++ mailman/scripts/hooks/post_restore.sh
@@ -6,9 +6,9 @@ docker-compose up --detach postgres
 flapctl hooks wait_ready postgres
 
 docker exec --user postgres flap_postgres psql --command "DROP DATABASE mailman;"
-docker exec --user postgres flap_postgres createdb -O mailman -E UTF8 -T template0 mailman
-
-docker exec --user postgres flap_postgres psql -c "CREATE EXTENSION pg_trgm;" mailman
-docker exec --user postgres flap_postgres psql -c "CREATE EXTENSION unaccent;" mailman
+docker exec --user postgres flap_postgres psql --command "CREATE DATABASE mailman WITH OWNER mailman;"
 
 gzip --decompress --stdout "$FLAP_DATA/mailman/backup.sql.gz" | docker exec --interactive --user postgres flap_postgres psql --dbname mailman
+
+docker-compose up --detach mailman
+flapctl hooks wait_ready mailman
