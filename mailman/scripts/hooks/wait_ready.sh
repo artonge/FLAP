38bbbaf6b080c54362e021c82d8021f2c6b0d8e1
Louis Chemineau <louis@chmn.me>
2021-04-08 20:35:12 +0200

    Improve mailman config

--- mailman/scripts/hooks/wait_ready.sh
+++ mailman/scripts/hooks/wait_ready.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-until docker-compose exec mailman_web mailman status | grep "[uWSGI] getting INI configuration from /opt/mailman-web/uwsgi.ini" > /dev/null
+until docker-compose logs mailman_web | grep "getting INI configuration from /opt/mailman-web/uwsgi.ini" > /dev/null
 do
     echo "Mailman web is unavailable - sleeping"
     sleep 1
