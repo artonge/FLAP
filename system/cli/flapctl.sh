ac4d6d509d2549449a7b615f2cad9725013a2886
Louis Chemineau <louis@chmn.me>
2021-04-11 20:01:46 +0200

    Fix typo

--- system/cli/flapctl.sh
+++ system/cli/flapctl.sh
@@ -53,7 +53,7 @@ fi
 
 # Execute the $CMD.
 # --line-buffered allow grep line by line output instead of grep using a larger buffer.
-# --color=always allow for the color not to be overrided.
+# --color=always allow for the color not to be overridden.
 "$FLAP_DIR/system/cli/cmd/$CMD.sh" "${ARGS[@]}" 2> /dev/stdout | \
 	sed --unbuffered -E "s/\* \[([a-z:]+)\]/\* \[$CMD:\1\]/" | \
 	grep --line-buffered --color=always -E "^\* \[.+\].*|$" | \
