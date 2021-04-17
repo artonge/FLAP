c5a92544a575dd9e69f1d1a0ec31606cb986c545
Louis Chemineau <louis@chmn.me>
2021-04-17 18:27:32 +0200

    Add missing version comment in migration script

--- matrix/scripts/migrations/6.sh
+++ matrix/scripts/migrations/6.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+# v1.14.7
+
 echo "* [6] Use environment variables for MATRIX_DOMAIN_NAME."
 echo "export MATRIX_DOMAIN_NAME=$MATRIX_DOMAIN_NAME" >> "$FLAP_DATA/system/flapctl.env"
 rm "$FLAP_DATA/matrix/domain.txt"
