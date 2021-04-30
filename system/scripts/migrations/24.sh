476e19d059c76b600b0705e1d592e89bf6d4397d
Louis Chemineau <louis@chmn.me>
2021-04-30 15:48:12 +0200

    Improve system's migration #24

--- system/scripts/migrations/24.sh
+++ system/scripts/migrations/24.sh
@@ -5,5 +5,12 @@ set -eux
 # Version v1.14.7
 
 echo "* [24] Update certbot hooks."
-echo "flapctl stop nginx" > /etc/letsencrypt/renewal-hooks/pre/stop_flap.sh
-echo "flapctl start nginx" > /etc/letsencrypt/renewal-hooks/post/start_flap.sh
+if [ -f /etc/letsencrypt/renewal-hooks/pre/stop_flap.sh ]
+then
+	echo "flapctl stop nginx" > /etc/letsencrypt/renewal-hooks/pre/stop_flap.sh
+fi
+
+if [ -f /etc/letsencrypt/renewal-hooks/post/start_flap.sh ]
+then
+	echo "flapctl start nginx" > /etc/letsencrypt/renewal-hooks/post/start_flap.sh
+fi
