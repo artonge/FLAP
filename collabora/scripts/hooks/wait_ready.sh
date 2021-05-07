ff5199e065e48b19af3c7d8b76659c053b81af0b
Louis Chemineau <louis@chmn.me>
2021-05-07 11:59:50 +0200

    Add collabora

---
+++ collabora/scripts/hooks/wait_ready.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eu
+
+# until docker-compose logs collabora | grep "Application startup complete." > /dev/null
+# do
+#     debug "Collabora is unavailable - sleeping"
+#     sleep 1
+# done
