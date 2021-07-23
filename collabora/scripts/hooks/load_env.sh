050c1f2149be44a489641efcfc03906218f2e05a
Louis Chemineau <louis@chmn.me>
2021-07-23 12:34:56 +0200

    Limit CA option to local domain provider

--- collabora/scripts/hooks/load_env.sh
+++ collabora/scripts/hooks/load_env.sh
@@ -3,3 +3,12 @@
 set -eu
 
 SUBDOMAINS="$SUBDOMAINS office"
+
+export COLLABORA_EXTRA_PARAMS
+COLLABORA_EXTRA_PARAMS='--o:welcome.enable=false --o:ssl.cert_file_path=/etc/letsencrypt/live/flap/fullchain.pem --o:ssl.key_file_path=/etc/letsencrypt/live/flap/privkey.pem'
+
+primary_domain_provider=$(cat "$FLAP_DATA/system/data/domains/$PRIMARY_DOMAIN_NAME/provider.txt")
+if [ "$primary_domain_provider" == "localhost" ] && [ "$primary_domain_provider" == "local" ]
+then
+	COLLABORA_EXTRA_PARAMS+='--o:ssl.ca_file_path=/etc/letsencrypt/live/flap/root.cer'
+fi
--- collabora/scripts/hooks/should_install.sh
+++ collabora/scripts/hooks/should_install.sh
@@ -4,4 +4,3 @@ set -eu
 
 test "${ENABLE_COLLABORA:-false}" == "true"
 test "$ARCH" == "x86_64"
-# test "${PRIMARY_DOMAIN_NAME:-}" != ""
