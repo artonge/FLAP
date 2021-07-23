a451485af39f75e86f400533c5dbbc4f752d9c72
Louis Chemineau <louis@chmn.me>
2021-07-23 14:28:23 +0200

    Remove debug code

--- collabora/scripts/hooks/wait_ready.sh
+++ collabora/scripts/hooks/wait_ready.sh
@@ -2,16 +2,7 @@
 
 set -eu
 
-i=1
 until docker-compose logs collabora | grep "Read to accept connections on port 9980." > /dev/null
 do
-    (( i++ ))
-    if [ "$i" == "50" ]
-    then
-        docker-compose logs collabora
-        exit 1
-    fi
-
     debug "Collabora is unavailable - sleeping"
-    sleep 2
 done
