e70c78f1d1c6933f2d47edaa31aee370a005738d
Louis Chemineau <louis@chmn.me>
2021-09-03 18:00:52 +0200

    Fix nextcloud migration #10

--- nextcloud/scripts/migrations/10.sh
+++ nextcloud/scripts/migrations/10.sh
@@ -4,7 +4,7 @@ set -eu
 
 # v1.15.0
 
-if [ "${FLAG_NO_DOCUMENTSERVER:-false}" == "true" ]
+if [ "${FLAG_NO_DOCUMENTSERVER:-false}" == "false" ]
 then
 	echo "* [10] Migrate from onlyoffice to collabora."
 	echo "export ENABLE_COLLABORA=true" >> "$FLAP_DATA"/system/flapctl.env
@@ -14,8 +14,16 @@ then
 	docker-compose exec -T --user www-data nextcloud php occ app:disable documentserver_community
 	docker-compose exec -T --user www-data nextcloud php occ app:disable onlyoffice
 
-	flapctl hooks post_install nextcloud
-	flapctl hooks post_update nextcloud
+	docker-compose exec -T --user www-data nextcloud php occ app:install richdocuments
+
+	php occ --quiet config:app:set richdocuments wopi_url --value "https://office.$PRIMARY_DOMAIN_NAME"
+	php occ --quiet config:app:set richdocuments public_wopi_url --value "https://office.$PRIMARY_DOMAIN_NAME"
+	php occ --quiet config:app:set richdocuments disable_certificate_verification --value "no"
+
+	if [ "$PRIMARY_DOMAIN_NAME" == "flap.test" ]
+	then
+		php occ --quiet config:app:set richdocuments disable_certificate_verification --value "yes"
+	fi
 
 	docker-compose --no-ansi down
 fi
