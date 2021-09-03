5edba7ff4ead6b059657fb58f93812c66425f875
Louis Chemineau <louis@chmn.me>
2021-09-03 17:39:12 +0200

    Use SSL termination

--- collabora/scripts/hooks/load_env.sh
+++ collabora/scripts/hooks/load_env.sh
@@ -6,10 +6,5 @@ FLAP_ENV_VARS="$FLAP_ENV_VARS \${COLLABORA_EXTRA_PARAMS}"
 SUBDOMAINS="$SUBDOMAINS office"
 
 export COLLABORA_EXTRA_PARAMS
-COLLABORA_EXTRA_PARAMS='--o:welcome.enable=false --o:ssl.cert_file_path=/etc/letsencrypt/live/flap/fullchain.pem --o:ssl.key_file_path=/etc/letsencrypt/live/flap/privkey.pem'
 
-primary_domain_provider=$(cat "$FLAP_DATA/system/data/domains/$PRIMARY_DOMAIN_NAME/provider.txt")
-if [ "$primary_domain_provider" == "localhost" ] || [ "$primary_domain_provider" == "local" ]
-then
-	COLLABORA_EXTRA_PARAMS+='--o:ssl.ca_file_path=/etc/letsencrypt/live/flap/root.cer'
-fi
+COLLABORA_EXTRA_PARAMS='--o:welcome.enable=false --o:ssl.enable=false --o:ssl.termination=true'
