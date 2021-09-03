b715cfe888dfec7a9b6b5edf0600a9d5658a39e8
Louis Chemineau <louis@chmn.me>
2021-09-03 17:38:41 +0200

    Fix wait ready script string

--- collabora/scripts/hooks/wait_ready.sh
+++ collabora/scripts/hooks/wait_ready.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-until docker-compose logs collabora | grep "Read to accept connections on port 9980." > /dev/null
+until docker-compose logs collabora | grep "Ready to accept connections on port 9980." > /dev/null
 do
     debug "Collabora is unavailable - sleeping"
 done
