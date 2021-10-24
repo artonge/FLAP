5976a2d4fa891de9c9e407fc23c5ae26474e0b8e
Louis Chemineau <louis@chmn.me>
2021-10-24 19:14:33 +0200

    Fix nextcloud migration #10

--- nextcloud/scripts/migrations/10.sh
+++ nextcloud/scripts/migrations/10.sh
@@ -4,6 +4,8 @@ set -eu
 
 # v1.20.0
 
+docker-compose --ansi never up --detach nextcloud
+
 docker-compose exec -T --user www-data nextcloud php occ config:system:set appstoreenabled --value true --type boolean
 
 if [ "${FLAG_NO_DOCUMENTSERVER:-false}" == "false" ]
@@ -15,18 +17,13 @@ then
 	# shellcheck disable=SC1091
 	source "$FLAP_DATA"/system/flapctl.env
 
-	docker-compose --ansi never up --detach nextcloud
 
 	docker-compose exec -T --user www-data nextcloud php occ app:disable documentserver_community
 	docker-compose exec -T --user www-data nextcloud php occ app:disable onlyoffice
-
-	docker-compose --ansi never down
 fi
 
 if [ "${ENABLE_COLLABORA:-false}" == "true" ]
 then
-	docker-compose --ansi never up --detach nextcloud
-
 	docker-compose exec -T --user www-data nextcloud php occ app:install richdocuments
 
 	docker-compose exec -T --user www-data nextcloud php occ config:app:set richdocuments wopi_url --value "https://office.$PRIMARY_DOMAIN_NAME"
@@ -37,6 +34,6 @@ then
 	then
 		docker-compose exec -T --user www-data nextcloud php occ --quiet config:app:set richdocuments disable_certificate_verification --value "yes"
 	fi
-
-	docker-compose --ansi never down
 fi
+
+docker-compose --ansi never down
