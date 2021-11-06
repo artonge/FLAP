2822b667247613f188b8e33f1c9174ac1df6a2c4
Louis Chemineau <louis@chmn.me>
2021-11-06 15:30:18 +0100

    Fix tmp file path to generate dkim

--- mail/scripts/hooks/post_domain_update.sh
+++ mail/scripts/hooks/post_domain_update.sh
@@ -4,12 +4,12 @@ set -eu
 
 debug "Generate DKIM."
 rm -rf "$FLAP_DIR/mail/config/opendkim"
-echo "" > "$FLAP_DIR/mail/config/vhost.tmp"
+echo "" > "$FLAP_DIR/mail/config/vhost.dkim.tmp"
 for domain in $DOMAIN_NAMES
 do
-    echo "$domain" >> "$FLAP_DIR/mail/config/vhost.tmp"
+    echo "$domain" >> "$FLAP_DIR/mail/config/vhost.dkim.tmp"
 done
-docker-compose exec -T mail cp /tmp/docker-mailserver/vhost.tmp /tmp/vhost.tmp
+docker-compose exec -T mail cp /tmp/docker-mailserver/vhost.dkim.tmp /tmp/vhost.dkim.tmp
 docker-compose exec -T mail open-dkim
 
 debug "Update DNS records."
