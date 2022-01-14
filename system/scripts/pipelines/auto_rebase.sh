8a58081406f8874031901a06e7cd4bab7819cbe7
Louis Chemineau <louis@chmn.me>
2022-01-14 17:40:12 +0100

    Fix usage of env var in auto-rebase

--- system/scripts/pipelines/auto_rebase.sh
+++ system/scripts/pipelines/auto_rebase.sh
@@ -2,10 +2,9 @@
 
 set -eu
 
-gitlab_base_url=https://gitlab.com/api/v4/projects
+opened_merge_requests=$(curl -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/merge_requests?state=opened)
 
-opened_merge_requests=$(curl -H "Authorization: Bearer $GITLAB_PERSONAL_TOKEN" $gitlab_base_url/"$PROJECT_ID"/merge_requests?state=opened)
-
-for iid in $(echo "$opened_merge_requests" | jq '.[] | .iid'); do
-	curl -X PUT -H "Authorization: Bearer $GITLAB_PERSONAL_TOKEN" $gitlab_base_url/"$PROJECT_ID"/merge_requests/"$iid"/rebase
+for iid in $(echo "$opened_merge_requests" | jq '.[] | .iid')
+do
+	curl -X PUT -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/merge_requests/"$iid"/rebase
 done
