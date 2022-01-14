4989a40c04f6b3d16c5415768e0b2718d3a113b9
Louis Chemineau <louis@chmn.me>
2022-01-14 18:12:02 +0100

    Remove auto-rebase wokflow

--- system/scripts/pipelines/auto_rebase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-for iid in $(curl --silent https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/merge_requests?state=opened | jq '.[] | .iid')
-do
-	curl -X PUT -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/merge_requests/"$iid"/rebase
-done
