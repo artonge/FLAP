aa9e3af70546ea3e3ac5ad7352c49aeb46d2ccfc
Louis Chemineau <louis@chmn.me>
2022-01-14 18:19:43 +0100

    Check disk space for FLAP_DIR

--- system/cli/lib/checkup/disk_space.sh
+++ system/cli/lib/checkup/disk_space.sh
@@ -2,35 +2,44 @@
 
 set -eu
 
-device=$(df -h --output=source,size,used,avail,pcent,file,target "$FLAP_DATA" | grep "$FLAP_DATA"):
-
-source=$(echo "$device" | awk '{print $1}')
-size=$(echo "$device" | awk '{print $2}')
-used=$(echo "$device" | awk '{print $3}')
-avail=$(echo "$device" | awk '{print $4}' | cut -d 'G' -f1)
-pcent=$(echo "$device" | awk '{print $5}' | cut -d '%' -f1)
-
 exit_code=0
 
-if [[ "$pcent" -gt "95" ]]
-then
-	echo "- Server free storage is less than 5%."
-	exit_code=1
-fi
+function checkSpace() {
+	path="$1"
+	path_exit_code=0
 
-if [[ "$avail" -lt "10" ]]
-then
-	echo "- Server free storage is less than 10 Go."
-	exit_code=1
-fi
+	device=$(df -h --output=source,size,used,avail,pcent,file,target "$path" | grep "$path"):
 
-if [ "$exit_code" == 1 ]
-then
-	echo "	- Source: $source"
-	echo "	- Size: $size"
-	echo "	- Used: $used"
-	echo "	- Avail: ${avail}G"
-	echo "	- Percent: ${pcent}%"
-fi
+	source=$(echo "$device" | awk '{print $1}')
+	size=$(echo "$device" | awk '{print $2}')
+	used=$(echo "$device" | awk '{print $3}')
+	avail=$(echo "$device" | awk '{print $4}' | cut -d 'G' -f1)
+	pcent=$(echo "$device" | awk '{print $5}' | cut -d '%' -f1)
+
+	if [[ "$pcent" -gt "95" ]]
+	then
+		echo "- Server free storage is less than 5% for $path."
+		path_exit_code=1
+	fi
+
+	if [[ "$avail" -lt "10" ]]
+	then
+		echo "- Server free storage is less than 10 Go for $path."
+		path_exit_code=1
+	fi
+
+	if [ "$path_exit_code" == 1 ]
+	then
+		exit_code=1
+		echo "	- Source: $source"
+		echo "	- Size: $size"
+		echo "	- Used: $used"
+		echo "	- Avail: ${avail}G"
+		echo "	- Percent: ${pcent}%"
+	fi
+}
+
+checkSpace "$FLAP_DATA"
+checkSpace "$FLAP_DIR"
 
 exit $exit_code
