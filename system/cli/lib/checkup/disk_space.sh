4c7b794ada23697f443ae3faa7e89f2a348fdc25
Louis Chemineau <louis@chmn.me>
2022-02-04 19:09:54 +0100

    Don't use human readble format in disk_space checkup

--- system/cli/lib/checkup/disk_space.sh
+++ system/cli/lib/checkup/disk_space.sh
@@ -8,12 +8,12 @@ function checkSpace() {
 	path="$1"
 	path_exit_code=0
 
-	device=$(df -h --output=source,size,used,avail,pcent,file,target "$path" | grep "$path"):
+	device=$(df --output=source,size,used,avail,pcent,file,target "$path" | grep "$path"):
 
 	source=$(echo "$device" | awk '{print $1}')
 	size=$(echo "$device" | awk '{print $2}')
 	used=$(echo "$device" | awk '{print $3}')
-	avail=$(echo "$device" | awk '{print $4}' | cut -d 'G' -f1)
+	avail=$(echo "$device" | awk '{print $4}')
 	pcent=$(echo "$device" | awk '{print $5}' | cut -d '%' -f1)
 
 	if [[ "$pcent" -gt "95" ]]
@@ -22,7 +22,7 @@ function checkSpace() {
 		path_exit_code=1
 	fi
 
-	if [[ "$avail" -lt "10" ]]
+	if [[ "$avail" -lt "10000000" ]]
 	then
 		echo "- Server free storage is less than 10 Go for $path."
 		path_exit_code=1
