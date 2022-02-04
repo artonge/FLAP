705facf07e82f8060f493679abd94c99b4c71180
Louis Chemineau <louis@chmn.me>
2022-02-04 11:14:32 +0100

    Convert wait_ready scripts to is_up

--- matomo/scripts/hooks/wait_ready.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-until docker-compose logs matomo | grep --quiet "NOTICE: ready to handle connections"
-do
-    debug "Matomo is unavailable - sleeping"
-    sleep 1
-done
--- peertube/scripts/hooks/wait_ready.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-until docker-compose logs peertube | grep "listening on 0.0.0.0:9000" > /dev/null
-do
-    debug "Peertube is unavailable - sleeping"
-    sleep 1
-done
